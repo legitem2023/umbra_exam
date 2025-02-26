44818865dd98b6c0cea353d8e8a286efbdae5d4f
Robert Marquez <legitem2023@gmail.com>
2025-02-27 01:16:19 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,21 +47,21 @@ const Home: React.FC = () => {
           placeholder="Task title"
           value={newTask.title}
           onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-          className="border p-2 mb-3 w-[100%] "
+          className="border p-2 mb-2 w-[100%] "
         />
         <input
           type="text"
           placeholder="Description"
           value={newTask.description}
           onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
-          className="border p-2 mb-3 w-[100%]"
+          className="border p-2 mb-2 w-[100%]"
         />
         <input
           type="date"
           placeholder="Date"
           defaultValue={newTask.dueDate}
           onChange={(e) => setNewTask({ ...newTask, dueDate: new Date(e.target.value).toDateString() })}
-          className="border p-2 mb-3 w-[50%]"
+          className="border p-2 mb-2 w-[50%]"
         />
         <button onClick={handleAddTask} className="bg-blue-500 border text-white p-2 w-[50%] rounded-3">
           Add Task
