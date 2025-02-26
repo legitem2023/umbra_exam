a13d884f88171a1953629e37ad499cf4e0fbde96
Robert Marquez <legitem2023@gmail.com>
2025-02-27 01:41:51 +0800

    Update Task.tsx

--- src/app/components/Task.tsx
+++ src/app/components/Task.tsx
@@ -50,12 +50,12 @@ const Task: React.FC<TaskProps> = ({
           {columns==='Done'?(<Done/>):(<></>)}
 <div className="absolute top-0 right-0 m-4">
 <ThreeDotMenu 
-  edit={()=>(
+  child1={()=>(
         <button onClick={onEdit} className="text-blue-500 hover:text-blue-700">
             <Icon icon="tabler:edit" width="24" height="24"  style={{color: "#f49f00"}} />
         </button> 
   )}
-  delete={()=>(
+  child2={()=>(
       <button onClick={onDelete} className="text-red-500 hover:text-red-700">
             <Icon icon="ph-trash-fill" width="24" height="24"  style={{color: "#f40000"}} />
       </button>
