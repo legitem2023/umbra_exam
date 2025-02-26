e9ae0eb443e88f8a00da052737f0c09f13908bd8
Robert Marquez <legitem2023@gmail.com>
2025-02-27 00:37:20 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,14 +47,14 @@ const Home: React.FC = () => {
           placeholder="Task title"
           value={newTask.title}
           onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-          className="border p-2 m-1 w-[100%]"
+          className="border box-content p-2 m-1 w-[100%] "
         />
         <input
           type="text"
           placeholder="Description"
           value={newTask.description}
           onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
-          className="border p-2 m-1 w-[100%]"
+          className="border box-content p-2 m-1 w-[100%]"
         />
         <input
           type="date"
