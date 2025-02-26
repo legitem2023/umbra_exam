c2af40c54a4b7cee55b3dd494e474e5b8215d5a0
Robert Marquez <legitem2023@gmail.com>
2025-02-27 07:43:49 +0800

    Update ThreeDotMenu.tsx

--- src/app/components/UI/ThreeDotMenu.tsx
+++ src/app/components/UI/ThreeDotMenu.tsx
@@ -29,10 +29,10 @@ const ThreeDotMenu:FC<ThreeDotProps> = ({child1,child2}) => {
       {isOpen && (
         <div className="absolute right-0 w-40 bg-white border border-gray-200 rounded-md shadow-lg">
           <ul className="py-1">
-            <li className="p-2">
+            <li className="p-1 flex flex-col">
               {child1()}
             </li>
-            <li className="p-2">
+            <li className="p-1 flex flex-col">
               {child2()}
             </li>
           </ul>
