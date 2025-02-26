510b7e7d193a66d7b3b7ba7d10bb372a38d50bc9
Robert Marquez <legitem2023@gmail.com>
2025-02-26 22:35:08 +0800

    Update Nav.tsx

--- src/app/components/Partial/Nav.tsx
+++ src/app/components/Partial/Nav.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className='Nav bg-blue-500 p-5'>
-        <Image src='https://www.umbradigitalcompany.com/images/general/logo-white.svg' height={100} width={100} alt='1'/>
+        <Image src='https://www.umbradigitalcompany.com/images/general/logo-white.svg' height={60} width={60} alt='1'/>
     </div>
   )
 }
